a2deadf3c68e9ede637da62b8c20ad9cf9a29d06
B, Ravi <RaB@mast.com>
2022-10-05 08:50:36 +0530

    initial push

---
+++ q1/script.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+>vendorloop
+cat vendor.log| while read line
+do 
+   vendor=`echo $line|cut -d ',' -f4`
+   echo $vendor >> vendorloop
+   if !grep -w $vendor vendorloop > /dev/null 2>&1
+   then 
+        true
+   else
+       grep $vendor vendor.log > ${VendorName}.log
+   fi
+done
+
+sort vendorloop | uniq | while read vendorname
+do 
+  conn=0
+  value=`cat ${vendorname}.log | cut -d',' -f5` 
+  set $value
+  i=0
+  for log1 in $value
+  do
+    conn=`expr $conn + $log1`
+    i=$(( i + 1 ))
+    if [ "$i" -eq "$#" ]
+    then 
+       echo "VendorName: $vendorname, TotalConnections: $conn"
+    else
+       true
+    fi
+  done
+done
+
+rm -f vendorloop
